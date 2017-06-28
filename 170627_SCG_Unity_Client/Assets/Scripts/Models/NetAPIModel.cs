2f0d362270104d496b75ceeb844f2b915e7ba9c6
DESKTOP-MGHFEMN\ScottLin <scoutlinn@gmail.com>
2017-06-28 18:14:05 +0800

    RESTFul relative.

---
+++ 170627_SCG_Unity_Client/Assets/Scripts/Models/NetAPIModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NetAPIModelNamespace
+{
+    public class NetAPIModelModel
+    {
+        public bool GetNodejsVersion(string version, string bundle)
+        {
+            bool rt = false;
+
+            //Call RESTFul
+
+
+            return rt;
+        }
+    }
+}
---
+++ 170627_SCG_Unity_Client/Assets/Scripts/Models/NetModel.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using UnityEngine;
+
+namespace NetModelNamespace
+{
+    public class NetModel
+    {
+        public enum NetType
+        {
+            None,
+            Socket,
+            WebSocket,
+            SocketIO,
+            Http,
+            Https,
+            Http2
+        }
+
+        private static NetType mNetType = NetType.None;
+        private static NetModel mNetModel = null;
+
+        public static NetModel GetInstance(NetType netType)
+        {
+            if (netType == NetType.None)
+            {
+                return null;
+            }
+            else
+            {
+                mNetType = netType;
+
+                if (mNetModel == null)
+                {
+                    mNetModel = new NetModel();
+                }
+
+                return mNetModel;
+            }
+        }
+
+        public bool RESTFulGET(string url, string JSON)
+        {
+            bool rt = false;
+
+            using (WebClient mWebClient = new WebClient())
+            {
+                ////No Form
+                ////Start
+                //string mRESP_JSON = mWebClient.UploadString(url, JSON);
+                ////End
+
+                //Form
+                //Start
+                NameValueCollection mREQ_param = new NameValueCollection();
+                mREQ_param.Add("JSON", JSON);
+                byte[] byte_RESP_param = mWebClient.UploadValues(url, mREQ_param);
+                string mRESP_JSON = Encoding.UTF8.GetString(byte_RESP_param);
+                //End
+            }
+
+            return rt;
+        }
+
+        public bool RESTFulPOST(string url, string JSON)
+        {
+            bool rt = false;
+
+            return rt;
+        }
+    }
+}
