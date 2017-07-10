ab8459c7fcea540409007528dd8ec2aa825ba005
DESKTOP-MGHFEMN\ScottLin <scoutlinn@gmail.com>
2017-07-10 17:46:22 +0800

    Update data struct and Coroutine RESTFul

---
+++ 170627_SCG_Unity_Client/Assets/000_Common/RegistTable/RegistTable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegistTable
+{
+    public class View
+    {
+        public RESTFulTestView mRESTFulTestView = null;
+    }
+
+
+
+    public static RegistTable mRegistTable = null;
+
+    public View mView;
+
+    public static RegistTable Instance
+    {
+        get
+        {
+            if(mRegistTable == null)
+            {
+                mRegistTable = new RegistTable();
+                mRegistTable.mView = new View();
+            }
+            return mRegistTable;
+        }
+    }
+}
---
+++ 170627_SCG_Unity_Client/Assets/002_Test/Scripts/Views/RESTFulTestView.cs
@@ -0,0 +1,90 @@
+﻿using SCG_Unity_Client_API;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RESTFulTestView : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        RegistTable.Instance.mView.mRESTFulTestView = this;
+        Debug.Log("Yeah");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public NetModel.NetType mNetType;
+
+    public void OnSetNetTypeButtonClick()
+    {
+        NetAPIModelModel.GetInstance().SetNetType(this.mNetType);
+    }
+
+    public void OnHelloWorldButtonClick()
+    {
+
+
+
+        StartCoroutine(Coroutine_OnHelloWorldButtonClick());
+        
+
+
+        //PacketStruct.Package_HelloWorld mPackage_HelloWorld = new PacketStruct.Package_HelloWorld();
+        //mPackage_HelloWorld.count = 0;
+        //mPackage_HelloWorld.text = string.Empty;
+
+
+        //string mRESP_JSON = NetAPIModelModel.GetInstance().HelloWorldTest(mPackage_HelloWorld);
+
+        //mPackage_HelloWorld = JsonUtility.FromJson<PacketStruct.Package_HelloWorld>(mRESP_JSON);
+
+        //Debug.Log("count: " + mPackage_HelloWorld.count);
+        //Debug.Log("text: " + mPackage_HelloWorld.text);
+    }
+
+    public IEnumerator Coroutine_OnHelloWorldButtonClick()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        //formData.Add( new MultipartFormDataSection("field1=foo&field2=bar") );
+        //formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+
+        PacketStruct.Package_Version mPackage_Version = new PacketStruct.Package_Version();
+        mPackage_Version.version = "1.0";
+        mPackage_Version.bundle = "1";
+
+        string json = JsonUtility.ToJson(mPackage_Version);
+
+        formData.Add(new MultipartFormDataSection("HEADER", "ServerVersion"));
+        formData.Add(new MultipartFormDataSection("JSON", json));
+
+        UnityWebRequest mUWR = UnityWebRequest.Post("http://10.10.10.171:3000/ServerVersion", formData);
+        yield return mUWR.Send();
+
+        byte[] byte_RESP_param = mUWR.downloadHandler.data;
+        string mRESP_JSON = Encoding.UTF8.GetString(byte_RESP_param);
+
+        Debug.Log("mRESP_JSON: " + mRESP_JSON);
+    }
+
+    public void OnGetServerVersionButtonClick()
+    {
+        PacketStruct.Package_Version mPackage_Version = new PacketStruct.Package_Version();
+        mPackage_Version.version = "testVersion";
+        mPackage_Version.bundle = "testBundle";
+
+
+        string mRESP_JSON = NetAPIModelModel.GetInstance().GetServerVersion(mPackage_Version);
+
+        mPackage_Version = JsonUtility.FromJson<PacketStruct.Package_Version>(mRESP_JSON);
+
+        Debug.Log("version: " + mPackage_Version.version);
+        Debug.Log("Bundle: " + mPackage_Version.bundle);
+    }
+}
